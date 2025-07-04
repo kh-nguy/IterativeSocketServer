57b8d6d17781daa11a67da728fa57bde4468ee15
kh-nguy <khnguy@proton.me>
2025-07-03 22:18:02 -0400

    added ability to continuously spawn client requests until user forcefully quits

--- src/MultiThreadedClient.java
+++ src/MultiThreadedClient.java
@@ -59,58 +59,60 @@ public class MultiThreadedClient
         int port = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("\nEnter command (Date and Time, Uptime, Memory Use, Netstat, Current Users, Running Processes): ");
-        String command = scanner.nextLine();
-
+        boolean run = true;
+        boolean valid = false;
         int count = 0;
         int[] allowed = {1, 5, 10, 15, 20, 25};
-        boolean valid = false;
-        // while loop to validate number of requests to generate
-        while (!valid) {
-            System.out.println("\nEnter number of client requests to generate (1, 5, 10, 15, 20, or 25):");
-            if (scanner.hasNextInt()) {
-                count = scanner.nextInt();
-                scanner.nextLine(); // consume newline character
-                for (int a : allowed) {
-                    if (count == a) {
-                        valid = true;
-                        break;
-                    }
+        String command = null;
+        while (run) {
+            System.out.println("\nEnter command (Date and Time, Uptime, Memory Use, Netstat, Current Users, Running Processes, Quit): ");
+            command = scanner.nextLine();
+            if (command.equalsIgnoreCase("Quit") || command.equalsIgnoreCase("quit")) {
+                System.out.println("Quitting client program ...");
+                run = false;
+                break;
+            }
+            // while loop to validate number of requests to generate
+            while (!valid) {
+                System.out.println("\nEnter number of client requests to generate (1, 5, 10, 15, 20, or 25):");
+                if (scanner.hasNextInt()) {
+                    count = scanner.nextInt();
+                    scanner.nextLine(); // consume newline character
+                    for (int a : allowed) {
+                        if (count == a) {
+                            valid = true;
+                            break;
+                        } // end if
+                    } // end enhanced for
+                    if (!valid) {
+                        System.out.println("Invalid input. Please enter one of the allowed values (1, 5, 10, 15, 20, or 25).");
+                    } // end if
+                } else {
+                    System.out.println("Please enter a valid number.");
+                    scanner.nextLine(); // consume invalid input
+                } // end else-if
+            } // end while loop for client requests
+            long totalTime = 0;
+            Thread[] threads = new Thread[count];
+            for (int i = 0; i < count; i++) {
+                threads[i] = new ClientTask(address, port, command);
+                long start = System.currentTimeMillis();
+                threads[i].start();
+                try {
+                    threads[i].join();
+                } catch (InterruptedException e) {
+                    System.out.println("Error: " + e.getMessage());
                 }
-                if (!valid) {
-                    System.out.println("Invalid input. Please enter one of the allowed values (1, 5, 10, 15, 20, or 25).");
-                }
-            } else {
-                System.out.println("Invalid input. Please enter a valid number.");
-                scanner.nextLine(); // consume invalid input
+                long end = System.currentTimeMillis();
+                totalTime += (end - start);
             }
-        }
-
-        long totalTime = 0;
-        Thread[] threads = new Thread[count];
-
-        for (int i = 0; i < count; i++)
-        {
-            threads[i] = new ClientTask(address, port, command);
-            long start = System.currentTimeMillis();
-            threads[i].start();
-
-            try
-            {
-                threads[i].join();
-            }
-            catch (InterruptedException e)
-            {
-                System.out.println("Error: " + e.getMessage());
-            }
-
-            long end = System.currentTimeMillis();
-            totalTime += (end - start);
-        }
-        System.out.println("Total Turn-around Time: " + totalTime + " ms");
-        System.out.println("Average Turn-around Time: " + (totalTime / (double) count) + " ms");
-
+            if (run) {
+                System.out.println("Total turn-around time: " + (double) totalTime + " ms");
+                System.out.println("Average turn-around time: " + (totalTime / (double) count) + " ms");
+                totalTime = 0;
+                valid = false;
+            } // end if
+        } // end while loop for command input
         scanner.close();
-
     } //end main
 } //end MultiThreaded Client class
